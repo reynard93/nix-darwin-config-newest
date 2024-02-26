87496e548f5b7140bfd1203a4f9f6d024cd0013a
Reylee <reynard_lee_from.tp@mom.gov.sg>
2024-02-26 14:17:31 +0800

    remove declarative dock setting, fix cargo path to point to bin, no need to manually set cargo home and rustup home

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -30,9 +30,7 @@ let name = "Reylee";
     export PATH=$HOME/.npm-packages/bin:$HOME/bin:$PATH
     export PATH=$HOME/.local/share/bin:$PATH
     export PNPM_HOME=~/.pnpm-packages
-    export CARGO_HOME=$HOME/.cargo # was manually installed with brew
-    export RUSTUP_HOME=HOME/.rustup # think no need but no harm
-    export PATH=$CARGO_HOME:$RUSTUP_HOME:$PATH
+    export PATH=$HOME/.cargo/bin:$PATH 
 
     # Remove history data we don't want to see
     export HISTIGNORE="pwd:ls:cd"
