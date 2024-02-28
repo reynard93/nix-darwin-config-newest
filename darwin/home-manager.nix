ec117e60bbff84c56d60e4824af5dc470704d788
Reylee <reynard_lee_from.tp@mom.gov.sg>
2024-02-28 11:37:26 +0800

    revert back to emacs-macport i rly need more the windows to work properly

--- darwin/home-manager.nix
+++ darwin/home-manager.nix
@@ -54,34 +54,7 @@ in
       programs = {
         emacs = {
           enable = true;
-          package = pkgs.emacs-unstable-pgtk;
-          extraPackages = epkgs: [epkgs.vterm];
-          overrides = final: prev: {
-            # https://github.com/jcf/home-manager-restart-emacs
-            patches =
-              (prev.patches or [])
-              ++ [
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-                  sha256 = "0c41rgpi19vr9ai740g09lka3nkjk48ppqyqdnncjrkfgvm2710z";
-                })
-                # Use poll instead of select to get file descriptors
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-                  sha256 = "0j26n6yma4n5wh4klikza6bjnzrmz6zihgcsdx36pn3vbfnaqbh5";
-                })
-                # Enable rounded window with no decoration
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
-                  sha256 = "0x187xvjakm2730d1wcqbz2sny07238mabh5d97fah4qal7zhlbl";
-                })
-                # Make Emacs aware of OS-level light/dark mode
-                (pkgs.fetchpatch {
-                  url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
-                  sha256 = "14ndp2fqqc95s70fwhpxq58y8qqj4gzvvffp77snm2xk76c1bvnn";
-                })
-              ];
-          };
+          package = pkgs.emacs-macport;
         };
       } // import ../shared/home-manager.nix { inherit config pkgs lib; };
 
