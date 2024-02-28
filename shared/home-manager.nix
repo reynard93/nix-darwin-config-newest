eb3ad2138d5393a90c0a3f3cec442217aa8cba0d
Reylee <reynard_lee_from.tp@mom.gov.sg>
2024-02-28 14:51:28 +0800

    remove git config maybe will fix magit?

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -78,35 +78,6 @@ let name = "Reylee";
     alias lg=lazygit
   '';
 
-  git = {
-    enable = true;
-    ignores = [ "*.swp" ];
-    userName = name;
-    userEmail = email;
-    lfs = {
-      enable = true;
-    };
-    aliases = {
-     aco = "add -A && commit -m";
-     a = "!git add .";
-     d = "diff";
-     co = "checkout";
-     ci = "commit";
-     ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-     pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-     st = "status";
-     br = "branch";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      core = { 
-        autocrlf = "input";
-      };
-      pull.rebase = true;
-      rebase.autoStash = true;
-    };
-  };
-
   ssh = {
     enable = true;
 
