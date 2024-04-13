b0b0848ee7df7d3c954767af0fd866798eae113a
Reynard TW <reynardtw@Reynards-MacBook-Pro.local>
2024-04-13 23:57:10 +0800

    remove ssh setting

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -67,24 +67,4 @@ let name = "Reylee";
     alias lg=lazygit
   '';
 
-  ssh = {
-    enable = true;
-
-    extraConfig = lib.mkMerge [
-      ''
-        Include ~/.orbstack/ssh/config
-        Host github.com
-          Hostname github.com
-          IdentitiesOnly yes
-      ''
-      (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-        ''
-          IdentityFile /home/${user}/.ssh/id_github
-        '')
-      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-        ''
-          IdentityFile /Users/${user}/.ssh/id_github
-        '')
-    ];
-  };
 }
